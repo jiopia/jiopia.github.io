1854b4fd9abad4ec8c9d4ad21675b93f2c9e5ee3
jiopia <2518883@qq.com>
2017-08-07 11:00:50 +0800

    Site updated: 2017-08-07 11:00:50

--- photos/ins.js
+++ photos/ins.js
@@ -112,8 +112,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://litten.me/ins/' + data.link[i];
+          var minSrc = 'http://jio.ee/ins-min/' + data.link[i] + '.min.jpg';
+          var src = 'http://jio.ee/ins/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
